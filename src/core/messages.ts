53fdd77f06d9a91ff0ed05abc91fab0b7c92934d
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2023-11-11 12:36:20 +0700

    add messages api

---
+++ src/core/messages.ts
@@ -0,0 +1,14 @@
+import { getAllItems, patchItems, saveItems } from "./database";
+import type { IMessage } from "bootstrap-chat-ui";
+
+export function getAllMessages(db: IDBDatabase): Promise<IMessage[]> {
+  return getAllItems(db, "messages");
+}
+
+export function saveMessages(db: IDBDatabase, messages: IMessage[]) {
+  return saveItems(db, "messages", messages);
+}
+
+export function patchMessages(db: IDBDatabase, messages: IMessage[]) {
+  return patchItems(db, "messages", messages);
+}
